b6b25f6f746661cee8ee4eac632ebee06c4f227e
zxr <1523871672@qq.com>
2025-12-03 20:40:21 +0800

    1. 修改前端，实现基础注册以及登录功能 2. 完善日志系统，现有日志会分类并统一打印到logs文件 3. 整理项目文件结构

--- Note_back/src/main/java/com/riptidez/notebackend/auth/service/impl/AuthServiceImpl.java
+++ Note_back/src/main/java/com/riptidez/notebackend/auth/service/impl/AuthServiceImpl.java
@@ -4,12 +4,15 @@ import com.riptidez.notebackend.auth.entity.User;
 import com.riptidez.notebackend.auth.mapper.UserMapper;
 import com.riptidez.notebackend.auth.service.AuthService;
 import com.riptidez.notebackend.exception.ExceptionWithMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AuthServiceImpl implements AuthService {
+    private final static Logger log = LoggerFactory.getLogger(AuthServiceImpl.class);
 
     @Autowired
     private UserMapper userMapper;
@@ -21,25 +24,31 @@ public class AuthServiceImpl implements AuthService {
     @Override
     @Transactional
     public User register(String username, String passwordHash) {
+        log.info("尝试注册用户...");
         if (userMapper.findHashByUsername(username) != null) {
+            log.warn("用户名已存在，注册失败");
             throw new ExceptionWithMessage("用户名已存在");
         }
 
         User user = new User();
         user.setUsername(username);
         user.setPasswordHash(passwordHash);
-        user.setNoteStructure("{ }");
+        user.setNoteStructure("{\"version\":1,\"userId\":1,\"root\":{\"id\":\"root\",\"name\":\"root\",\"type\":\"folder\",\"children\":[]}}");
         userMapper.insert(user);
+        log.info("注册成功");
 
         return user;
     }
 
     @Override
     public User login(String username, String passwordHash) {
+        log.info("尝试登录用户{}", username);
         User user = userMapper.findHashByUsername(username);
         if (user == null || !passwordHash.equals(user.getPasswordHash())) {
+            log.warn("登录失败，{}不存在", username);
             throw new ExceptionWithMessage("用户名或密码错误");
         }
+        log.info("成功登陆{}", username);
         return user;
     }
 
